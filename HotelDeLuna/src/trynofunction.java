3f864831ab18260184549c40b5731d704016a659
Nelson <Nelson@DESKTOP-JBGIVMA>
2024-01-13 15:13:36 +0800

    Eso pay gus

--- HotelDeLuna/src/trynofunction.java
+++ HotelDeLuna/src/trynofunction.java
@@ -1,4 +1,11 @@
+import javax.swing.JFrame;
 
 public class trynofunction {
-
+public static void main(String[] args) {
+	System.out.println("Hi Aaron");
+	
+	JFrame frame = new JFrame();
+	frame.setVisible(true);
+	
+}
 }
