6f2821ad4249b6a3009dfe7ad5620c68d6357d3b
User <User@MSI>
2024-01-13 15:46:15 +0800

    I added the size and text on the Frame

--- HotelDeLuna/src/trynofunction.java
+++ HotelDeLuna/src/trynofunction.java
@@ -1,4 +1,5 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class trynofunction {
 	public trynofunction() {
@@ -9,6 +10,11 @@ public static void main(String[] args) {
 	System.out.println("Hi Aaron");
 	
 	JFrame frame = new JFrame("AAron");
+	JLabel halo = new JLabel("HALO!");
+	
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.add(halo);
+	frame.setBounds(100,100,500,500);
 	frame.setVisible(true);
 	
 }
