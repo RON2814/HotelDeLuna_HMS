d69df0a0721a3d8b934ee855ea6482c5963093fa
Admin <Admin@DESKTOP-0PRI5IC>
2024-01-18 05:47:45 -0800

    dashboardUi

---
+++ HotelDeLuna/src/components/RoundedButton.java
@@ -0,0 +1,40 @@
+package components;
+
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JButton;
+
+class RoundedButton extends JButton {
+
+    public RoundedButton(String text) {
+        super(text);
+        setContentAreaFilled(false);
+        setFocusable(false);
+        setForeground(new Color(255,255,255));
+        setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        setBorder(null);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (getModel().isArmed()) {
+            g.setColor(getBackground().darker());
+        } else {
+            g.setColor(getBackground());
+        }
+
+        // Draw rounded button
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 50, 50);
+
+        super.paintComponent(g);
+    }
+}
+
